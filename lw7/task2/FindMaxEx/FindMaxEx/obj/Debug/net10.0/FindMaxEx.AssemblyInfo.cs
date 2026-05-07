62ba9a0216a47d1fb9dded0297c766300890d713
Potapenko Max <mpotapenko.mail@gmail.com>
2026-05-07 17:06:35 +0300

    feat/utils

--- lw7/task2/FindMaxEx/FindMaxEx/obj/Debug/net10.0/FindMaxEx.AssemblyInfo.cs
+++ lw7/task2/FindMaxEx/FindMaxEx/obj/Debug/net10.0/FindMaxEx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindMaxEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6dc579aa76101cc1a2539aa6229032fba96d73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f37b1fbfe6fb4d11f05e7fff7c50b88222fc6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindMaxEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindMaxEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
